535607ff6d22f7a5475341575f5dbc7ac90080b2
CaioMariins <caio.m.sousa@gmail.com>
2022-05-09 12:58:15 -0300

    lit, functio,branch

--- mod_1_intro/function.py
+++ mod_1_intro/function.py
@@ -1,15 +1,12 @@
-def check_n(n):
-    """
-    O retorno duplo do print +  a string é uma solução para evitar lidar com os nones do pytest
-    e ao mesmo tempo imprimir para o usuário a mensagem necessária.
-    
-    """
-
-    if (n % 5 == 0) and (n % 7 == 0):
+def check_n(n_n):
+    """Retorna a frase correspondente ao número."""
+    if (n_n % 5 == 0) and (n_n % 7 == 0):
         return print("fizzbuzz"), str("fizzbuzz")
-    elif (n % 7 == 0):
+    if n_n % 7 == 0:
         return print("buzz"), str("buzz")
-    elif (n % 5 == 0):
+    if n_n % 5 == 0:
         return print("fizz"), str("fizz")
-    elif (n % 5 !=0) and (n % 7 !=0):
+    if (n_n % 5 !=0) and (n_n % 7 !=0):
         return print("miss"), str("miss")
+    return None
+    
